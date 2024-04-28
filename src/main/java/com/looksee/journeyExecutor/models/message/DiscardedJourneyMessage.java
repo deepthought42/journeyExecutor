cab07db74e935777fd1fb0bf2fcc0572ac28cf91
Brandon Kindred <brandon.kindred@gmail.com>
2024-04-27 22:38:55 -0400

    refactored journey messages

--- src/main/java/com/looksee/journeyExecutor/models/message/DiscardedJourneyMessage.java
+++ src/main/java/com/looksee/journeyExecutor/models/message/DiscardedJourneyMessage.java
@@ -3,47 +3,32 @@ package com.looksee.journeyExecutor.models.message;
 import com.looksee.journeyExecutor.models.enums.BrowserType;
 import com.looksee.journeyExecutor.models.journeys.Journey;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class DiscardedJourneyMessage extends DomainAuditMessage {
 
+	@Getter
+	@Setter
 	private Journey journey;
+	
+	@Getter
+	@Setter
 	private BrowserType browserType;
-	private long domainId;
+
+	@Getter
+	@Setter
+	private long auditRecordId;
    
 	public DiscardedJourneyMessage() {}
 	
 	public DiscardedJourneyMessage(Journey journey, 
 								   BrowserType browserType, 
-								   long domainId, 
 								   long accountId, 
 								   long auditRecordId) {
 		super(accountId, auditRecordId);
 		setJourney(journey);
 		setBrowserType(browserType);
-		setDomainId(domainId);
+		setAuditRecordId(auditRecordId);
 	}
-
-	public BrowserType getBrowserType() {
-		return browserType;
-	}
-
-	public void setBrowserType(BrowserType browserType) {
-		this.browserType = browserType;
-	}
-
-	public long getDomainId() {
-		return domainId;
-	}
-
-	public void setDomainId(long domainId) {
-		this.domainId = domainId;
-	}
-
-	public Journey getJourney() {
-		return journey;
-	}
-
-	public void setJourney(Journey journey) {
-		this.journey = journey;
-	}
-
 }
